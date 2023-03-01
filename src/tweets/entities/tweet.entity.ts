aaaa7f6c4478f281e203432a16816670bc8e8e8b
Joshua Chisolm <jkchisolm22@gmail.com>
2023-03-01 00:09:44 -0800

    feat: complete documentation for tweet routes

--- src/tweets/entities/tweet.entity.ts
+++ src/tweets/entities/tweet.entity.ts
@@ -17,15 +17,37 @@ export class Tweet {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 'Hello World!',
+    description: 'The text content of a tweet.',
+  })
   @Column('text')
   body: string;
 
+  @ApiProperty({
+    example: new Date(),
+    description: "The tweet's creation date.",
+  })
   @CreateDateColumn()
   created_at: Date;
 
+  // @ApiProperty({
+  //   example: {
+  //     id: 1,
+  //     email: 'john@doe.com',
+  //     name: 'John Doe',
+  //     handle: 'johnDoe',
+  //     created_at: '2021-01-01T00:00:00.000Z',
+  //   },
+  //   description: "The tweet's author.",
+  // })
   @ManyToOne(() => User, (user) => user.posts)
   author: User;
 
+  @ApiProperty({
+    example: 1,
+    description: "The tweet's author's id.",
+  })
   @Column()
   authorId: number;
 }
--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -13,6 +13,11 @@ export class TweetsController {
   @Post()
   @ApiOperation({ summary: 'Creates a new tweet' })
   @ApiBody({ type: CreateTweetDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Created a new tweet',
+    type: Tweet,
+  })
   create(@Body() createPostDto: CreateTweetDto) {
     return this.tweetsService.create(createPostDto);
   }
@@ -28,11 +33,23 @@ export class TweetsController {
     return this.tweetsService.findAll();
   }
 
+  @ApiOperation({ summary: 'Returns a single tweet' })
+  @ApiResponse({
+    status: 200,
+    description: 'A single tweet',
+    type: Tweet,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tweetsService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'Deletes a tweet' })
+  @ApiResponse({
+    status: 200,
+    description: 'The deleted tweet',
+    type: Tweet,
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.tweetsService.remove(+id);
